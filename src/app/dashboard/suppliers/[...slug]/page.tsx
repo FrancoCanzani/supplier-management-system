4dd2a2d343bf7b22181cda4b10e4ff094f5b803f
Franco Canzani <francocanzani@gmail.com>
2024-01-20 00:01:08 +0100

    replaces generate static params with catch all

--- src/app/dashboard/suppliers/[...slug]/page.tsx
+++ src/app/dashboard/suppliers/[...slug]/page.tsx
@@ -7,20 +7,6 @@ import dbConnect from '@/lib/database/dbConnect';
 import { Task } from '@/lib/database/schemas/taskSchema';
 import SupplierInformation from '@/components/supplierInformation';
 
-export async function generateStaticParams() {
-  const res = await fetch(`https://${process.env.VERCEL_URL}/api/suppliers`);
-
-  if (!res.ok) {
-    throw new Error(`Error: ${res.statusText}`);
-  }
-
-  const suppliers = await res.json();
-
-  return suppliers.map((supplier: SupplierProps) => ({
-    slug: supplier.id,
-  }));
-}
-
 export default async function SupplierPage({
   params,
 }: {
