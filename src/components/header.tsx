3f85b446a73e6ba8bb995eaa3d6701943b543004
Franco Canzani <francocanzani@gmail.com>
2024-02-05 23:37:22 +0100

    adds order to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,6 +23,12 @@ export default function Header() {
         >
           Suppliers
         </Link>
+        <Link
+          href={'/dashboard/orders'}
+          className='inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50 h-10 px-4 py-2 w-full justify-start'
+        >
+          Orders
+        </Link>
       </nav>
       <UserButton afterSignOutUrl='/' />
     </header>
